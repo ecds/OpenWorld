284c9452b3d30190a7d4076dfa0afb8f21114d77
Yang Li <yang@truman.edu>
2020-10-05 17:54:40 -0400

    add members and refactored the className

--- src/containers/Components/Credit/index.js
+++ src/containers/Components/Credit/index.js
@@ -34,7 +34,7 @@ class Credit extends React.Component {
               color: ${THEME.TEXT} !important;
             }
 
-            .owa-about-sponsorship-link {
+            .owa-about-link {
               color: ${THEME.MAIN} !important;
             }
 
@@ -146,7 +146,7 @@ class Credit extends React.Component {
                       <a
                         href="https://ecds.emory.edu/"
                         alt="Emory Center for Digital Scholarship"
-                        className="owa-about-sponsorship-link"
+                        className="owa-about-link"
                       >
                         Emory Center for Digital Scholarship (ECDS), Emory
                         University
@@ -185,7 +185,7 @@ class Credit extends React.Component {
                       <a
                         href="https://kazirangauniversity.in/"
                         alt="Kaziranga University"
-                        className="owa-about-sponsorship-link"
+                        className="owa-about-link"
                       >
                         Center for Virtual Reality, Kaziranga University
                       </a>
@@ -208,7 +208,7 @@ class Credit extends React.Component {
                       <a
                         href="https://www.unifesp.br/"
                         alt="Universidade Federal de São Paulo"
-                        className="owa-about-sponsorship-link"
+                        className="owa-about-link"
                       >
                         School of Philosophy, Letters and Human Sciences (EFLCH)
                         and the Institute of Science and Technology (ICT),
@@ -233,7 +233,7 @@ class Credit extends React.Component {
                       <a
                         href="https://www.yonsei.ac.kr/"
                         alt="Yonsei University"
-                        className="owa-about-sponsorship-link"
+                        className="owa-about-link"
                       >
                         Department of Information and Interaction Design, Yonsei
                         University
@@ -243,7 +243,92 @@ class Credit extends React.Component {
                   </div>
                 </div>
               </Tab>
-              <Tab eventKey="members" title="Members"></Tab>
+              <Tab eventKey="members" title="Members">
+                <div className="row owa-sponsorship-row">
+                  <div className="col-12">
+                    <div className="owa-about-title">
+                      <a
+                        href="https://ecds.emory.edu/about/staff/basu-arya.html"
+                        alt="Arya Basu, PhD"
+                        className="owa-about-link"
+                      >
+                        Arya Basu, PhD
+                      </a>
+                    </div>
+                    <p>
+                      Arya creates, modifies, and configures 3D models using a
+                      variety of computer modeling, simulation software, and
+                      geospatial data. In partnership with faculty and ECDS
+                      staff, Arya prepares aesthetically composed digital media
+                      through graphic design, image processing, and data
+                      visualization for use in ECDS-supported digital
+                      scholarship projects. He also researches and explores new
+                      methods of visualizing data, including 3D, virtual
+                      reality, and augmented reality.
+                    </p>
+                  </div>
+                </div>
+
+                <div className="row owa-sponsorship-row">
+                  <div className="col-12">
+                    <div className="owa-about-title">
+                      <div>Chris Wyllie</div>
+                    </div>
+                    <p>
+                      Chris was a computer science student at Emory University.
+                      He developed the initial version of the site using React,
+                      Leaflet, and Bootstrap.
+                    </p>
+                  </div>
+                </div>
+
+                <div className="row owa-sponsorship-row">
+                  <div className="col-12">
+                    <div className="owa-about-title">
+                      <a
+                        href="https://ecds.emory.edu/about/staff/page-michael.html"
+                        alt="Michael Page"
+                        className="owa-about-link"
+                      >
+                        Michael Page
+                      </a>
+                    </div>
+                    <p>
+                      Michael manages map and data libraries, GIS
+                      infrastructure, and consults on research projects that
+                      have a geospatial technology/spatial data component. His
+                      primary research focus involves cartography, geospatial
+                      technologies, and urban geography, and his key projects
+                      include American Excavations Samothrace, Georgia Coast
+                      Atlas, and OpenWorld Atlanta. He is coauthor of Sacred
+                      Places: A Guide to the Civil Rights Movement in Atlanta,
+                      Georgia. His maps have been published in many print and
+                      digital journals and books.
+                    </p>
+                  </div>
+                </div>
+
+                <div className="row owa-sponsorship-row">
+                  <div className="col-12">
+                    <div className="owa-about-title">
+                      <a
+                        href="https://ecds.emory.edu/about/staff/li-yang.html"
+                        alt="Yang Li"
+                        className="owa-about-link"
+                      >
+                        Yang Li
+                      </a>
+                    </div>
+                    <p>
+                      Yang is a Senior Software Engineer at the Emory Center for
+                      Digital Scholarship and an Information Designer at the Fox
+                      Center for Humanistic Inquiries. He has been the UI/UX
+                      designer for this project since 2019 and completed visual
+                      design for the current site.
+                    </p>
+                  </div>
+                </div>
+              </Tab>
             </Tabs>
           </ModalBody>
           <ModalFooter>
@@ -251,7 +336,7 @@ class Credit extends React.Component {
             <a
               href="https://ecds.emory.edu"
               alt="Emory Center for Digital Scholarship"
-              className="owa-about-sponsorship-link"
+              className="owa-about-link"
             >
               Emory Center for Digital Scholarship
             </a>
