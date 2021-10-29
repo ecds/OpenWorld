6afc2a0ecc7c0cbe8b4f768c9820fffb62efbca3
Jay Varner <jayvarner@gmail.com>
2021-10-29 09:18:36 -0400

    Export classes to make component more reusable

--- src/containers/Components/GenericLayer/index.js
+++ src/containers/Components/GenericLayer/index.js
@@ -4,7 +4,7 @@ import { FaCheckSquare, FaRegSquare } from 'react-icons/fa';
 
 import Spinner from '../Spinner';
 
-const Container = styled.div`
+export const Container = styled.div`
     border: none;
     border-radius: 0;
     box-shadow: none;
@@ -16,50 +16,59 @@ const Container = styled.div`
     }
 `;
 
-const Label = styled.span`
+export const Label = styled.label`
     font-weight: bold;
     position: relative;
     left: 8px;
-    color: ${props => props.active ? props.theme.MAIN : props.theme.TEXT};
+    color: ${props => props.active ? props.activeColor : props.theme.TEXT};
 `;
 
-const Description = styled.p`
+export const Description = styled.p`
     position: relative;
     left: 24px;
     margin-bottom: 0;
 `;
 
-const Icon = styled.i`
+export const Icon = styled.i`
     color: ${props => props.active ? props.theme.MAIN : props.theme.TEXT};
 `;
 
-const Attribution = styled(Description)`
+export const Attribution = styled(Description)`
     font-style: italic;
 `;
 
 export default class GenericLayer extends React.Component {
+
+    componentDidUpdate(prevState) {
+        if ((this.props.groupActive !== prevState.groupActive) && this.props.groupActive !== this.props.active) {
+          this.props.onClick();
+      }
+    }
+
     render() {
         return (
             <Container>
-                <label htmlFor={`${this.props.id}-toggle`}>
-                    <input className="layer-checkbox" type="checkbox" id={`${this.props.id}-toggle`} checked={this.props.active} onChange={this.props.onClick} />
-                    <Icon active={this.props.active}>
-                        {this.props.loading ?
-                            <Spinner
-                                width={'16px'}
-                                height={'16px'}
-                                thickness={'4px'}
-                                margin={'2px auto auto auto'}
-                            /> :
-                            this.props.active ?
-                                <FaCheckSquare /> :
-                                <FaRegSquare />
-                        }
-                    </Icon>
-                    <Label active={this.props.active}>{this.props.title}</Label>
+                {/* <label> */}
+                    <Label htmlFor={`${this.props.id}-toggle`} active={this.props.active} activeColor={this.props.activeColor}>
+                        <input className="layer-checkbox" type="checkbox" id={`${this.props.id}-toggle`} checked={this.props.active} onChange={this.props.onClick} />
+                        <Icon active={this.props.active}>
+                            {this.props.loading ?
+                                <Spinner
+                                    width={'16px'}
+                                    height={'16px'}
+                                    thickness={'4px'}
+                                    margin={'2px auto auto auto'}
+                                /> :
+                                this.props.active ?
+                                    <FaCheckSquare /> :
+                                    <FaRegSquare />
+                            }
+                        </Icon>&nbsp;
+                        {this.props.title}
+                    </Label>
                     <Description>{this.props.desc}</Description>
                     <Attribution>{this.props.attr}</Attribution>
-                </label>
+                {/* </label> */}
             </Container>
         )
     }
