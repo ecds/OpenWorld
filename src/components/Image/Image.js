bf48bbe3f824a195696f86fe9ff2bda3b355e29d
Jay Varner <jayvarner@gmail.com>
2022-10-05 21:13:23 -0400

    Clean up

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -8,20 +8,19 @@ const Image = (props) => {
   const [show, setShow] = useState(false);
 
   return (
-  <>
-    <img src={props.source} alt={props.caption} className='d-block w-100 owa-image-button' onClick={() => setShow(true)} />
-    <Modal size="lg" show={show} onHide={() => setShow(false)}>
-      <Modal.Header closeButton></Modal.Header>
-        <Modal.Body className="mx-auto">
-          <img className="img-fluid" role="button" src={props.source} alt={props.caption} />
-        </Modal.Body>
-        <ModalFooter>
-          {props.caption}
-        </ModalFooter>
-    </Modal>
-  </>
+    <>
+      <img src={props.source} alt={props.caption} className='d-block w-100 owa-image-button' onClick={() => setShow(true)} />
+      <Modal size="lg" show={show} onHide={() => setShow(false)}>
+        <Modal.Header closeButton></Modal.Header>
+          <Modal.Body className="mx-auto">
+            <img className="img-fluid" role="button" src={props.source} alt={props.caption} />
+          </Modal.Body>
+          <ModalFooter>
+            {props.caption}
+          </ModalFooter>
+      </Modal>
+    </>
   )
-  };
-
+};
 
 export default Image;
