9cda4e9708c6d7de0dde36d4e2f9e55aa45724d2
Jay Varner <jayvarner@gmail.com>
2022-10-18 12:00:42 -0400

    Add info popover for base layers

--- src/components/Layers/TileLayers/TileLayers.js
+++ src/components/Layers/TileLayers/TileLayers.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useRef } from 'react';
 import { DomEvent } from 'leaflet';
 import { Accordion, Col, Container, Row, Toast, ToastContainer } from 'react-bootstrap';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Popover from 'react-bootstrap/Popover';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import './TileLayers.scss';
 import { tileLayers } from './data';
 import OpacityControl from '../../OpacityControl/OpacityControl';
@@ -24,6 +28,7 @@ const TileLayers = (props) => {
   }, [props, controlRef]);
 
   return (
+    <>
     <ToastContainer position="bottom-start" className="leaflet-bottom leaflet-left overflow-hidden" tabIndex="0">
       <Toast show={true} className="leaflet-control leaflet-bar overflow-auto" ref={controlRef}>
       <Accordion defaultActiveKey="0">
@@ -37,7 +42,25 @@ const TileLayers = (props) => {
                 <Container fluid key={index}>
                   <Row>
                     <Col>
-                      <h5>{layer.title}</h5>
+                      <h5 className="fs-5">
+                        {layer.title}&nbsp;
+                        <OverlayTrigger
+                          trigger={["focus"]}
+                          placement="right"
+                          overlay={
+                            <Popover>
+                              <Popover.Header as="h3">
+                                {layer.title}
+                              </Popover.Header>
+                              <Popover.Body>
+                                Stuff about layer
+                              </Popover.Body>
+                            </Popover>
+                          }
+                        >
+                          <FontAwesomeIcon className="fs-6 text-muted" icon={faInfoCircle} role="button" tabIndex="0" />
+                        </OverlayTrigger>
+                      </h5>
                     </Col>
                   </Row>
                   <OpacityControl layer={layer} year={props.year} />
@@ -50,6 +73,7 @@ const TileLayers = (props) => {
       </Accordion>
       </Toast>
     </ToastContainer>
+    </>
   );
 };
 
