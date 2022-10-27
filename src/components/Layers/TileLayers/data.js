9c7acee07fd2e8b3ceb9017bd20722ead587d7d0
Jay Varner <jayvarner@gmail.com>
2022-10-27 14:11:25 -0400

    Add attributions to tile layers

--- src/components/Layers/TileLayers/data.js
+++ src/components/Layers/TileLayers/data.js
@@ -1,33 +1,75 @@
 import L from 'leaflet';
 
+const modernStreetMapDescription = () => {
+  return (
+    <p>
+      &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>
+    </p>
+  )
+}
+
+const satelliteDescription = () => {
+  return (
+    <div>
+      <dl>
+        <dt>Imagery</dt>
+        <dd>
+          Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community.
+        </dd>
+        <dt>Labels</dt>
+        <dd>
+          &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>
+        </dd>
+      </dl>
+      <p>
+      </p>
+    </div>
+  );
+}
+
 export const tileLayers = [
   {
     urls: ['https://geoserver.ecds.emory.edu/Atlanta1878/gwc/service/wms?layers=Atlanta1878:q516x4&format=image/png&transparant=true'],
     leafletLayers: [
       L.tileLayer.wms('https://geoserver.ecds.emory.edu/Atlanta1878/gwc/service/wms?layers=Atlanta1878:q516x4&format=image/png&transparant=true', {
-        opacity: 0
+        opacity: 0,
+        maxZoom: 20
       })
     ],
     type: 'WMS',
     year: 1840,
-    title: '1848 City Atlas'
+    title: '1853 City Atlas',
+    description: 'Originally produced in 1853 for the City Council by civil engineer Edward Vincent. Includes index and circle marking the extended city limits. "These numbers are city numbers, and deeds refer to them--whether subdivisions or not. N.B. The City Line has been extended."',
+    researchLinks: [
+      'https://scholarblogs.emory.edu/woodruff/news/explore-marbls-digital-historic-map-collection',
+      'http://www.digitalgallery.emory.edu/luna/servlet/detail/EMORYUL~3~3~1000~100068:Old-Map-of-Atlanta?sort=Publication_Title%2CTitle%2CPage_No_%2CPages&qvq=sort:Publication_Title%2CTitle%2CPage_No_%2CPages;lc:EMORYUL%7E3%7E3&mi=0&trs=24',
+      'https://muse.jhu.edu/article/430839/pdf',
+      'https://en.wikipedia.org/wiki/Edward_A._Vincent',
+      'https://www.loc.gov/resource/g3924a.ct001134/'
+    ]
   },
   {
     urls: ['https://geoserver.ecds.emory.edu/ATLMaps/gwc/service/wms?layers=ATLMaps:2rwkdcdv&format=image/png&transparant=true'],
     leafletLayers: [
       L.tileLayer.wms('https://geoserver.ecds.emory.edu/ATLMaps/gwc/service/wms?layers=ATLMaps:2rwkdcdv&format=image/png&transparant=true', {
-        opacity: 0
+        opacity: 0,
+        maxZoom: 20
       })
     ],
     type: 'WMS',
     year: 1870,
-    title: '1870 City Map'
+    title: '1870 City Map',
+    description: 'Hanleiter\'s Directory Map of Atlanta, 1870.',
+    researchLinks: [
+      'https://dlg.usg.edu/record/gsu_afpl_26?canvas=0&x=1948&y=2577&w=5795'
+    ]
   },
   {
     urls: ['https://geoserver.ecds.emory.edu/ATLMaps/gwc/service/wms?layers=ATLMaps:2s4d022m&format=image/png&transparant=true'],
     leafletLayers: [
       L.tileLayer.wms('https://geoserver.ecds.emory.edu/ATLMaps/gwc/service/wms?layers=ATLMaps:2s4d022m&format=image/png&transparant=true', {
-        opacity: 0
+        opacity: 0,
+        maxZoom: 20
       })
     ],
     type: 'WMS',
@@ -35,9 +77,9 @@ export const tileLayers = [
     title: '1895 City Map'
   },
   {
-    urls: ['https://geoserver.ecds.emory.edu/ATLMaps/gwc/service/wms?layers=ATLMaps:Atlanta1906&format=image/png&transparant=true'],
+    urls: ['https://geoserver.ecds.emory.edu/ATLMaps/gwc/service/wms?layers=ATLMaps:2s6zg3zx&format=image/png&transparant=true'],
     leafletLayers: [
-      L.tileLayer.wms('https://geoserver.ecds.emory.edu/ATLMaps/gwc/service/wms?layers=ATLMaps:Atlanta1906&format=image/png&transparant=true', {
+      L.tileLayer.wms('https://geoserver.ecds.emory.edu/ATLMaps/gwc/service/wms?layers=ATLMaps:2s6zg3zx&format=image/png&transparant=true', {
         opacity: 0
       })
     ],
@@ -49,7 +91,8 @@ export const tileLayers = [
     urls: ['https://geoserver.ecds.emory.edu/ATLMaps/gwc/service/wms?layers=ATLMaps:sq4pd&format=image/png&transparant=true'],
     leafletLayers: [
       L.tileLayer.wms('https://geoserver.ecds.emory.edu/ATLMaps/gwc/service/wms?layers=ATLMaps:sq4pd&format=image/png&transparant=true', {
-        opacity: 0
+        opacity: 0,
+        maxZoom: 20
       })
     ],
     type: 'WMS',
@@ -63,23 +106,29 @@ export const tileLayers = [
       'https://s3.amazonaws.com/tilemaps/ATL28_200tiles/{z}/{x}/{y}.png'
     ],
     leafletLayers: [
-      L.tileLayer.wms('https://s3.amazonaws.com/tilemaps/ATL28_1000tiles/{z}/{x}/{y}.png', {
-        opacity: 0
-      }),
-      L.tileLayer.wms('https://s3.amazonaws.com/tilemaps/ATL28_200tiles/{z}/{x}/{y}.png', {
-        opacity: 0
+      // L.tileLayer.wms('https://s3.amazonaws.com/tilemaps/ATL28_1000tiles/{z}/{x}/{y}.png', {
+      //   opacity: 0
+      // }),
+      L.tileLayer.wms('https://geoserver.ecds.emory.edu/ATLMaps/gwc/service/wms', {
+        layers: 'ATLMaps:ATL28',
+        format: 'image/png',
+        transparent: true,
+        updateInterval: 1,
+        opacity: 0,
+        maxZoom: 20
       })
     ],
     bounds: [[33.93379544, -84.21603335], [33.63298531, -84.51696580]],
-    minZoom: 11,
-    maxNativeZoom: 16,
     title: '1928 City Atlas'
   },
   {
-    urls: ['https://geoserver.ecds.emory.edu/ATLMaps/gwc/service/wms?layers=ATLMaps:2s3w9vfd&format=image/png&transparant=true'],
+    urls: ['https://geoserver.ecds.emory.edu/ATLMaps/gwc/service/wms?layers=ATLMaps:2s3w9vfd&'],
     leafletLayers: [
-      L.tileLayer.wms('https://geoserver.ecds.emory.edu/ATLMaps/gwc/service/wms?layers=ATLMaps:2s3w9vfd&format=image/png&transparant=true', {
-        opacity: 0
+      L.tileLayer.wms('https://geoserver.ecds.emory.edu/ATLMaps/gwc/service/wms?layers=ATLMaps:2s3w9vfd', {
+        format: 'image/png',
+        transparent: true,
+        opacity: 0,
+        maxZoom: 20
       })
     ],
     type: 'WMS',
@@ -96,7 +145,8 @@ export const tileLayers = [
       })
     ],
     year: 2022,
-    title: 'Modern Street Map'
+    title: 'Modern Street Map',
+    description: modernStreetMapDescription()
   },
   {
     urls: [
@@ -112,6 +162,9 @@ export const tileLayers = [
       })
     ],
     year: 2022,
-    title: 'Modern Satellite Image'
+    title: 'Modern Satellite Image',
+    description: satelliteDescription()
   },
 ];
+
+export const YEARS = tileLayers.map(layer => { return layer.year });
