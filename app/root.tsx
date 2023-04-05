1aba9d0c2a0567a94509c0c352d1b38da55dc179
Jay Varner <jayvarner@gmail.com>
2023-04-05 08:46:57 -0400

    Add Terminal Station model

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { createContext, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import type { MetaFunction } from "@remix-run/node";
 import { useSearchParams } from "@remix-run/react";
 import {
@@ -30,17 +30,23 @@ export function links() {
   ];
 }
 
-
 export default function App() {
   const [searchParams, setSearchParams] = useSearchParams();
   const [mapState, setMapState] = useState<Map | undefined>(undefined);
   const [currentYearState, setCurrentYearState] = useState<number|undefined>(undefined);
-  const centerLng = searchParams.get("centerLng") ?? -84.3891;
-  const centerLat = searchParams.get("centerLat") ?? 33.7528;
-  const center = [centerLng, centerLat];
-  const zoom = parseFloat(searchParams.get("zoom")) ?? 11;
-  const pitch = parseFloat(searchParams.get("pitch")) ?? 0;
-  const bearing = parseFloat(searchParams.get("bearing")) ?? 0;
+  const center = [searchParams.get("centerLng") ?? -84.3891, searchParams.get("centerLat") ?? 33.7528];
+  const zoom = searchParams.get("zoom") ?? 11.0;
+  const pitch = searchParams.get("pitch") ?? 0.0;
+  const bearing = searchParams.get("bearing") ?? 0.0;
+
+  useEffect(() => {
+    mapState?.flyTo({
+      bearing,
+      center,
+      pitch,
+      zoom
+    })
+  }, [mapState, bearing, center, pitch, zoom]);
 
   return (
     <html lang="en">
